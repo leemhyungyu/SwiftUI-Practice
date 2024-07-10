2b5f0800bd9311df8546a70c21353b0da59673bf
leemhyungyu <lhglbkksj@naver.com>
2024-07-10 20:00:31 +0900

    feat: LandMark Model 구현

--- Chapter1/Chapter1/Models/Landmark.swift
+++ Chapter1/Chapter1/Models/Landmark.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreLocation
+
+struct LandMark: Hashable, Codable {
+    
+    private var imageName: String
+    private var coordinates: Coordinates
+    
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    var image: Image {
+        Image(imageName)
+    }
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latutude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latutude: Double
+        var longitude: Double
+    }
+}
