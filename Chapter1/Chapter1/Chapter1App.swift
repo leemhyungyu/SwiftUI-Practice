2ea37596206fe02017885052b77b92da8b417a63
leemhyungyu <lhglbkksj@naver.com>
2024-07-10 19:22:38 +0900

    chore: Chapter1 프로젝트 파일 추가

---
+++ Chapter1/Chapter1/Chapter1App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter1App.swift
+//  Chapter1
+//
+//  Created by 임현규 on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Chapter1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
