593645df38d04043fca5c81c21aa9d79e02ccd28
leemhyungyu <lhglbkksj@naver.com>
2024-07-11 21:28:59 +0900

    [Chapter3-2] Section1 - Display a user profile

--- Landmarks/Landmarks/View/Categories/CategoryHome.swift
+++ Landmarks/Landmarks/View/Categories/CategoryHome.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @Environment(ModelData.self) var modelData
-
+    @State private var showingProfile = false
 
     var body: some View {
         NavigationSplitView {
@@ -24,8 +24,21 @@ struct CategoryHome: View {
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key] ?? [])
                 }
+                .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environment(modelData)
+            }
         } detail: {
             Text("Select a Landmark")
         }
---
+++ Landmarks/Landmarks/View/Profiles/ProfileHost.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by 임현규 on 7/11/24.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+        .environment(ModelData())
+}
