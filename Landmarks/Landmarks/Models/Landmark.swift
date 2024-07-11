94a26486b2ff9dc838132466212a31fb1c782fa0
leemhyungyu <lhglbkksj@naver.com>
2024-07-11 19:27:54 +0900

    [Chapter3-1] Section2 - Create a category list

--- Landmarks/Landmarks/Models/Landmark.swift
+++ Landmarks/Landmarks/Models/Landmark.swift
@@ -19,7 +19,15 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var description: String
     var isFavorite: Bool
-
+    
+    var category: Category
+    
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+    
     var image: Image {
         Image(imageName)
     }
--- Landmarks/Landmarks/View/Categories/CategoryHome.swift
+++ Landmarks/Landmarks/View/Categories/CategoryHome.swift
@@ -8,18 +8,25 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+
+
     var body: some View {
-        
         NavigationSplitView {
-            Text("Hello, World!")
-                .navigationTitle("Featured")
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
         } detail: {
             Text("Select a Landmark")
         }
-
     }
 }
 
+
 #Preview {
     CategoryHome()
+        .environment(ModelData())
 }
