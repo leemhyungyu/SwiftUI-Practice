1754c24b0a211e931d243e7d29193c84f7780024
leemhyungyu <lhglbkksj@naver.com>
2024-07-13 12:31:21 +0900

    [Chapter4-1] Section1 - Create a view to represent a UIPageViewController

--- Landmarks/Landmarks/Models/Landmark.swift
+++ Landmarks/Landmarks/Models/Landmark.swift
@@ -10,37 +10,41 @@ import SwiftUI
 import CoreLocation
 
 struct Landmark: Hashable, Codable, Identifiable {
-    private var imageName: String
-    private var coordinates: Coordinates
-    
-    var id: Int
-    var name: String
-    var park: String
-    var state: String
-    var description: String
-    var isFavorite: Bool
-    var isFeatured: Bool
-    
-    var category: Category
-    
-    enum Category: String, CaseIterable, Codable {
-        case lakes = "Lakes"
-        case rivers = "Rivers"
-        case mountains = "Mountains"
-    }
-    
-    var image: Image {
-        Image(imageName)
-    }
-    
-    var locationCoordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(
-            latitude: coordinates.latitude,
-            longitude: coordinates.longitude)
-    }
-    
-    struct Coordinates: Hashable, Codable {
-        var latitude: Double
-        var longitude: Double
-    }
+  private var imageName: String
+  private var coordinates: Coordinates
+  
+  var id: Int
+  var name: String
+  var park: String
+  var state: String
+  var description: String
+  var isFavorite: Bool
+  var isFeatured: Bool
+  
+  var category: Category
+  
+  enum Category: String, CaseIterable, Codable {
+    case lakes = "Lakes"
+    case rivers = "Rivers"
+    case mountains = "Mountains"
+  }
+  
+  var image: Image {
+    Image(imageName)
+  }
+  
+  var featureImage: Image? {
+      isFeatured ? Image(imageName + "_feature") : nil
+  }
+  
+  var locationCoordinate: CLLocationCoordinate2D {
+    CLLocationCoordinate2D(
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude)
+  }
+  
+  struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+  }
 }
